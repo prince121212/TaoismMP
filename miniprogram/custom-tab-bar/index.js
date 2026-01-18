f6d1ea180cbf864c78dc057d264973c943761740
黄长委 <huangzhangwei@huomakeji.com>
2026-01-18 18:56:37 +0800

    fix: 修复自定义 TabBar 的 WASMContext 错误
    - 移除 attached() 生命周期中的 getCurrentPages() 调用
    - 移除 setSelected() 方法
    - 通过页面 onShow() 直接设置选中状态
    - 修复组件初始化错误

--- miniprogram/custom-tab-bar/index.js
+++ miniprogram/custom-tab-bar/index.js
@@ -14,33 +14,12 @@ Component({
       }
     ]
   },
-  
-  attached() {
-    // 组件加载时，根据当前页面设置选中状态
-    this.setSelected()
-  },
 
   methods: {
     switchTab(e) {
       const data = e.currentTarget.dataset
       const url = data.path
       wx.switchTab({ url })
-    },
-
-    setSelected() {
-      const pages = getCurrentPages()
-      const currentPage = pages[pages.length - 1]
-      const url = currentPage.route
-      
-      const selected = this.data.list.findIndex(item => {
-        return item.pagePath === `/${url}`
-      })
-      
-      if (selected !== -1) {
-        this.setData({
-          selected: selected
-        })
-      }
     }
   }
 })
